b424d2b78db96833a735c1998248ccfd404e989c
Wiktoria Wolczyk <wiki.99.wolczyk@gmail.com>
2024-09-04 00:27:31 +0200

    Created desktop view of productDetails

--- src/products/ProductDetails/ProductDetails.tsx
+++ src/products/ProductDetails/ProductDetails.tsx
@@ -42,7 +42,7 @@ const ProductDetails = () => {
 
   return (
     <div>
-      <div className="scrollableContainerForStepsInProductDetails">
+      {/* <div className="scrollableContainerForStepsInProductDetails">
         <div className="stepsInProductDetails">
           <span style={{ paddingRight: 10 }}>x-kom</span>{" "}
           <i className="fa-solid fa-chevron-right"></i>
@@ -59,106 +59,141 @@ const ProductDetails = () => {
           <span style={{ paddingRight: 10 }}>laptopy 13 i mniejsze</span>{" "}
           <i className="fa-solid fa-chevron-right"></i>
         </div>
-      </div>
-
-      <div className="containerForMainPhotoOfProduct">
-        <div className="divForMainProductPhoto">
-          <img src={data?.message.img} />
-        </div>
-      </div>
-      <div className="containerForNameOfProductAndPriceInProductDetails">
-        <div>{data?.message.name}</div>
-        <div className="PriceInProductDetails">
-          {data?.message.discountedPrice || data?.message.price} zł
-        </div>
-        <div className="ContainerForQuantityButtonsAndAddToCart">
-          <div className="containerForQuantityButtonsInProductDetails">
-            <QuantityChanger
-              quantity={quantityValue}
-              id={data?.message.id}
-              changeQuantity={changeQuantity}
-            />
-          </div>
-          <div className="divForButtonAddToCartInProductDetails">
-            <button className="buttonAddToCartInProductDetails">
-              <i
-                className="fa-solid fa-cart-shopping"
-                style={{ color: "#ffffff" }}
-              ></i>
-              <span style={{ marginLeft: 10 }}>Dodaj do koszyka</span>
-            </button>
+      </div> */}
+      <div className="mobile-desktop-productDetails">
+        <div className="containerForMainPhotoOfProduct">
+          <div className="divForMainProductPhoto">
+            <img src={data?.message.img} className="mainPhotoOfProduct" />
           </div>
         </div>
-      </div>
-      <div className="conatainerForDetailsAboutProductDelivery">
-        <div className="divWithDetailsAboutDelivery">
-          <div className="divForIMGInDetailsAboutProductDelivery">
-            <Available width={25} />
-          </div>
-          <div className=" divForTextInDetailsAboutProductDelivery">
-            <span className="greenFont">Dostępny</span>
-            <span className="smallText">Dowiedz się więcej</span>
-          </div>
-        </div>
-        <div className="divWithDetailsAboutDelivery">
-          <div className="divForIMGInDetailsAboutProductDelivery">
-            <Clock width={25} />
-          </div>
-          <div className=" divForTextInDetailsAboutProductDelivery">
-            <span>Kup teraz, otrzymasz jutro</span>
-            <span className="smallText">Zapłać w ciągu</span>
-          </div>
-        </div>
-        <div className="divWithDetailsAboutDelivery">
-          <div className="divForIMGInDetailsAboutProductDelivery">
-            <Bus width={25} />
-          </div>
-          <div className=" divForTextInDetailsAboutProductDelivery">
-            <span className="greenFont">Darmowa dostawa</span>
-            <span className="smallText">Sprawdź szczegóły</span>
-          </div>
-        </div>
-
-        <div className="divWithDetailsAboutDelivery">
-          <div className="divForIMGInDetailsAboutProductDelivery">
-            <Onplace width={25} />
-          </div>
-          <div className=" divForTextInDetailsAboutProductDelivery">
-            <span>Dostępny w salonach</span>
-            <span className="smallText">Wybierz salon</span>
-          </div>
-        </div>
-      </div>
-      <div className="containerForInsuranceInformation">
-        <span>Ubezpieczenie urządzenia</span>
-
-        <div className="containerForRadioInSelectInsurance">
-          <RadioGroup onChange={setValue} value={value}>
-            <Stack direction="column">
-              <div
-                className="divForRadioSelectInsurance"
-                style={{
-                  border:
-                    value === "1" ? "1px solid gray" : "0px solid transparent",
-                }}
-              >
-                <Radio value="1">Brak dodatkowej ochrony</Radio>
+        <div className="desktopProductDetails">
+          <div className="containerForNameOfProductAndPriceInProductDetails">
+            <div className="productName">{data?.message.name}</div>
+            <div className="PriceInProductDetails">
+              {data?.message.discountedPrice || data?.message.price} zł
+            </div>
+            <div className="ContainerForQuantityButtonsAndAddToCart displayNoneOnDesktop">
+              <div className="containerForQuantityButtonsInProductDetails">
+                <QuantityChanger
+                  quantity={quantityValue}
+                  id={data?.message.id}
+                  changeQuantity={changeQuantity}
+                />
               </div>
-              <div
-                className="divForRadioSelectInsurance"
-                style={{
-                  border:
-                    value === "2" ? "1px solid gray" : "0px solid transparent",
-                }}
-              >
-                <Radio value="2">wybierz eXtra ochronę</Radio>
+              <div className="divForButtonAddToCartInProductDetails">
+                <button className="buttonAddToCartInProductDetails">
+                  <i
+                    className="fa-solid fa-cart-shopping"
+                    style={{ color: "#ffffff" }}
+                  ></i>
+                  <span style={{ marginLeft: 10 }}>Dodaj do koszyka</span>
+                </button>
               </div>
-            </Stack>
-          </RadioGroup>
+            </div>
+          </div>
+
+          <div className="desktopviewDeliveryAndInsurance">
+            <div className="conatainerForDetailsAboutProductDelivery">
+              <div className="divWithDetailsAboutDeliveryOnDesktop">
+                <span className="divForPriceAboveQuantityButtons">
+                  {data?.message.discountedPrice || data?.message.price} zł
+                </span>
+                <div className="ContainerForQuantityButtonsAndAddToCart">
+                  <div className="containerForQuantityButtonsInProductDetails">
+                    <QuantityChanger
+                      quantity={quantityValue}
+                      id={data?.message.id}
+                      changeQuantity={changeQuantity}
+                    />
+                  </div>
+                  <div className="divForButtonAddToCartInProductDetails">
+                    <button className="buttonAddToCartInProductDetails">
+                      <i
+                        className="fa-solid fa-cart-shopping"
+                        style={{ color: "#ffffff" }}
+                      ></i>
+                      <span style={{ marginLeft: 10 }}>Dodaj do koszyka</span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="divWithDetailsAboutDelivery">
+                <div className="divForIMGInDetailsAboutProductDelivery">
+                  <Available width={25} />
+                </div>
+                <div className=" divForTextInDetailsAboutProductDelivery">
+                  <span className="greenFont">Dostępny</span>
+                  <span className="smallText">Dowiedz się więcej</span>
+                </div>
+              </div>
+              <div className="divWithDetailsAboutDelivery">
+                <div className="divForIMGInDetailsAboutProductDelivery">
+                  <Clock width={25} />
+                </div>
+                <div className=" divForTextInDetailsAboutProductDelivery">
+                  <span>Kup teraz, otrzymasz jutro</span>
+                  <span className="smallText">Zapłać w ciągu</span>
+                </div>
+              </div>
+              <div className="divWithDetailsAboutDelivery">
+                <div className="divForIMGInDetailsAboutProductDelivery">
+                  <Bus width={25} />
+                </div>
+                <div className=" divForTextInDetailsAboutProductDelivery">
+                  <span className="greenFont">Darmowa dostawa</span>
+                  <span className="smallText">Sprawdź szczegóły</span>
+                </div>
+              </div>
+
+              <div className="divWithDetailsAboutDelivery">
+                <div className="divForIMGInDetailsAboutProductDelivery">
+                  <Onplace width={25} />
+                </div>
+                <div className=" divForTextInDetailsAboutProductDelivery">
+                  <span>Dostępny w salonach</span>
+                  <span className="smallText">Wybierz salon</span>
+                </div>
+              </div>
+            </div>
+            <div className="containerForInsuranceInformation">
+              <span>Ubezpieczenie urządzenia</span>
+
+              <div className="containerForRadioInSelectInsurance">
+                <RadioGroup onChange={setValue} value={value}>
+                  <Stack direction="column">
+                    <div
+                      className="divForRadioSelectInsurance"
+                      style={{
+                        border:
+                          value === "1"
+                            ? "1px solid gray"
+                            : "0px solid transparent",
+                      }}
+                    >
+                      <Radio value="1">Brak dodatkowej ochrony</Radio>
+                    </div>
+                    <div
+                      className="divForRadioSelectInsurance"
+                      style={{
+                        border:
+                          value === "2"
+                            ? "1px solid gray"
+                            : "0px solid transparent",
+                      }}
+                    >
+                      <Radio value="2">wybierz eXtra ochronę</Radio>
+                    </div>
+                  </Stack>
+                </RadioGroup>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
       <div className="containerForDescriptionOfProduct">
-        <span style={{ fontSize: 16 }}>Opis: </span>
+        <span style={{ fontSize: 18 }}>Opis: </span>
         <div className="divForTextDescribingProduct">
           <span>
             Lorem Ipsum is simply dummy text of the printing and typesetting
